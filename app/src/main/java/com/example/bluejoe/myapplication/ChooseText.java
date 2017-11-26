4cb24ac98f4c0235930697e11172146fc3bfb407
Yunzhe <iLoveLive@honoka.moe>
2017-11-27 01:10:54 +0800

    File selection almost there.

--- app/src/main/java/com/example/bluejoe/myapplication/ChooseText.java
+++ app/src/main/java/com/example/bluejoe/myapplication/ChooseText.java
@@ -1,8 +1,16 @@
 package com.example.bluejoe.myapplication;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,14 +28,16 @@ import java.util.List;
 
 public class ChooseText extends AppCompatActivity {
 
+    private static final int FILE_SELECT_CODE = 2;
+    private static final String TAG = "ChooseText";
     private List<TextList> textList = new ArrayList<>();
+    String string;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_text);
         initTextList();
-        String string;
         Button button_choose = (Button) findViewById(R.id.button_choose);
         TextListAdapter adapter = new TextListAdapter(ChooseText.this,
                 R.layout.text_list_item, textList);
@@ -37,8 +47,12 @@ public class ChooseText extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 //                Choose a txt file
-                Intent intent = new Intent(ChooseText.this, MarkText.class);
-                startActivity(intent);
+                if (ContextCompat.checkSelfPermission(ChooseText.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ChooseText.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                    Log.d(TAG, "onClick: 000");
+                } else {
+                    openFileManager();
+                }
             }
         });
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -57,6 +71,69 @@ public class ChooseText extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void openFileManager() {
+        Log.d(TAG, "openFileManager: 222");
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("text/plain");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        try {
+            startActivityForResult( Intent.createChooser(intent, "Select a File to Upload"), FILE_SELECT_CODE);
+            Log.d(TAG, "openFileManager: 333");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "Please install a File Manager.",  Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult: 444");
+        String path = null;
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                Log.d(TAG, "onActivityResult: 555");
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    assert uri != null;
+                    if ("content".equalsIgnoreCase(uri.getScheme())) {
+                        String[] projection = { "_data" };
+                        Cursor cursor = getContentResolver().query(uri, projection,null, null, null);
+                        assert cursor != null;
+                        int column_index = cursor.getColumnIndexOrThrow("_data");
+                        if (cursor.moveToFirst()) {
+                            path =  cursor.getString(column_index);
+                            cursor.close();
+                        }
+                    } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+                        path = uri.getPath();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        // Read string from path
+        string = path;
+        Intent intent = new Intent(ChooseText.this, MarkText.class);
+        intent.putExtra("string", string);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: 111");
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openFileManager();
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     private void initTextList() {
--- app/src/main/java/com/example/bluejoe/myapplication/TextList.java
+++ app/src/main/java/com/example/bluejoe/myapplication/TextList.java
@@ -2,23 +2,24 @@ package com.example.bluejoe.myapplication;
 
 /**
  * Created by Yunzhe on 2017/11/26.
+ *
  */
 
-public class TextList {
+class TextList {
 
     private String name;
     private String filename;
 
-    public TextList(String name, String filename) {
+    TextList(String name, String filename) {
         this.name = name;
         this.filename = filename;
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public String getFilename() {
+    String getFilename() {
         return filename;
     }
 }
